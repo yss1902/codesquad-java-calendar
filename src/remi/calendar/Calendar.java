006cdc72b6804ca077b484dc5c6958816b0c8e62
remi <remi@59.0.141.171>
2022-11-03 17:21:03 +0900

    repeat fuction added

--- src/remi/calendar/Calendar.java
+++ src/remi/calendar/Calendar.java
@@ -3,6 +3,12 @@ package remi.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysofMonth(int month) {
+		return MAX_DAYS[month - 1];
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("일   월   화   수   목   금   토");
@@ -14,12 +20,19 @@ public class Calendar {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner sc = new Scanner(System.in);
-		System.out.println("달을 입력하세요.");
-		int month = sc.nextInt();
+		Calendar cal = new Calendar();
 
-		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
-		sc.close();
+		
+		System.out.println("반복 횟수를 입력하세요");
+		int repeat = sc.nextInt();
+		
+		for(int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = sc.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysofMonth(month));
+		}
+		System.out.println("BYE");
+
 
 	}
 
