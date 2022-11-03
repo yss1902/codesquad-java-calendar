9abb0761c53e3a2391e124ed0e38991d217bddfc
remi <remi@59.0.141.171>
2022-11-03 16:12:30 +0900

    update2

--- src/remi/calendar/Calendar.java
+++ src/remi/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package remi.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,6 +11,15 @@ public class Calendar {
 		System.out.println("8    9   10   11  12   13  14");
 		System.out.println("15   16  17   18  19   20  21");
 		System.out.println("22   23  24   25  26   27  28");
+
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner sc = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		int month = sc.nextInt();
+
+		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
+		sc.close();
 
 	}
 
